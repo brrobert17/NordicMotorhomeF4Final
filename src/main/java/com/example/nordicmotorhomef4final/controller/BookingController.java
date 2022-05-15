343b7f50785016a37350d7410fa96099fa684ecc
filippo <filippo-ceradini>
2022-05-15 15:48:36 +0200

    fixed Controllers

--- src/main/java/com/example/nordicmotorhomef4final/controller/BookingController.java
+++ src/main/java/com/example/nordicmotorhomef4final/controller/BookingController.java
@@ -1,12 +1,14 @@
 package com.example.nordicmotorhomef4final.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Controller
 public class BookingController {
 //    @Autowired
 //    BookingRepo bookingRepo;
--- src/main/java/com/example/nordicmotorhomef4final/controller/VehicleController.java
+++ src/main/java/com/example/nordicmotorhomef4final/controller/VehicleController.java
@@ -5,6 +5,7 @@ import com.example.nordicmotorhomef4final.model.Vehicle;
 import com.example.nordicmotorhomef4final.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Controller
 public class VehicleController {
 
     @Autowired
