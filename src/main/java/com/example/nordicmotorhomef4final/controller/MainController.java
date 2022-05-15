1a2cb245802a434f686e3ccd6b5e7b26b9988e92
zbahramzy <zalandbahramzy@gmail.com>
2022-05-15 14:49:25 +0200

    Created more packages in the template folder.

--- src/main/java/com/example/nordicmotorhomef4final/controller/MainController.java
+++ src/main/java/com/example/nordicmotorhomef4final/controller/MainController.java
@@ -3,7 +3,7 @@ package com.example.nordicmotorhomef4final.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
-public class Controller {
+public class MainController {
 
     @GetMapping("/")
     public String showIndex() {
