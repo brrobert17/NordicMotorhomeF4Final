a6368cfec615f7d5de669354a13d5d763223c25d
filippo <filippo-ceradini>
2022-05-15 16:17:52 +0200

    fixed Vehiclespage

--- src/main/java/com/example/nordicmotorhomef4final/controller/BookingController.java
+++ src/main/java/com/example/nordicmotorhomef4final/controller/BookingController.java
@@ -1,5 +1,7 @@
 package com.example.nordicmotorhomef4final.controller;
 
+import com.example.nordicmotorhomef4final.model.Booking;
+import com.example.nordicmotorhomef4final.repo.BookingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,31 +12,31 @@ import java.util.List;
 
 @Controller
 public class BookingController {
-//    @Autowired
-//    BookingRepo bookingRepo;
+    @Autowired
+    BookingRepo bookingRepo;
 
     //    Bookings html connector GET
-    @GetMapping("/templates/bookingsPage.html")
+    @GetMapping("booking/bookingsPage")
     public String showBooking(Model model) {
-//        List<Booking> listBooking = bookingRepo.findAll();
-//        model.addAttribute("listBooking", listBooking);
-//        model.addAttribute("bookinG", new Bookings());
-//        model.addAttribute("searchBook", new Bookings());
-        return "bookingsPage";
+        List<Booking> listBooking = bookingRepo.findAll();
+        model.addAttribute("listBooking", listBooking);
+        model.addAttribute("bookinG", new Booking());
+        model.addAttribute("searchBook", new Booking());
+        return "booking/bookingsPage";
     }
 
     //    Bookings html connector Post
-    @PostMapping("/templates/bookingsPage.html")
-    public String searchBooking(Model model){// Bookings bookings) {
-//        List<Bookings> listBooking = bookingRepo.findByRegistration(bookings.getRegistration().toString());
-//        if (bookings.getRegistration().toString().isEmpty()) {
-//            System.out.println("isempty");
-//            listBooking = bookingRepo.findAll();
-//        }
-//        model.addAttribute("listBooking", listBooking);
-//        model.addAttribute("bookinG", new Bookings());
-//        model.addAttribute("searchBook", new Bookings());
-        return "bookingsPage";
+    @PostMapping("booking/bookingsPage")
+    public String searchBooking(Model model, Booking bookings) {
+        List<Booking> listBooking = bookingRepo.findByVehicleRegistrationPlate(bookings.getVehicle().getRegistrationPlate().toString());
+        if (bookings.getVehicle().getRegistrationPlate().toString().isEmpty()) {
+            System.out.println("isempty");
+            listBooking = bookingRepo.findAll();
+        }
+        model.addAttribute("listBooking", listBooking);
+        model.addAttribute("bookinG", new Booking());
+        model.addAttribute("searchBook", new Booking());
+        return "booking/bookingsPage";
     }
 
 }
--- src/main/java/com/example/nordicmotorhomef4final/repo/BookingRepo.java
+++ src/main/java/com/example/nordicmotorhomef4final/repo/BookingRepo.java
@@ -1,4 +1,16 @@
 package com.example.nordicmotorhomef4final.repo;
 
-public interface BookingRepo {
+
+import com.example.nordicmotorhomef4final.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookingRepo extends JpaRepository<Booking, Long> {
+    List<Booking> findByVehicleRegistrationPlate(String s);
+    Optional<Booking> findByBookingId(int i);
+
 }
+
