97149463e5abc0ed38d9945a3c42233a458446ff
filippo <filippo-ceradini>
2022-05-15 14:08:00 +0200

    Removed Lombok

--- src/main/java/com/example/nordicmotorhomef4final/model/Vehicle.java
+++ src/main/java/com/example/nordicmotorhomef4final/model/Vehicle.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@Data
+
 @Entity
 @Table(name = "vehicles")
 public class Vehicle {
@@ -31,4 +31,60 @@ public class Vehicle {
 
     @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL)
     private List<Booking> bookings;
+
+    public String getRegistrationPlate() {
+        return registrationPlate;
+    }
+
+    public void setRegistrationPlate(String registrationPlate) {
+        this.registrationPlate = registrationPlate;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getcLicense() {
+        return cLicense;
+    }
+
+    public void setcLicense(String cLicense) {
+        this.cLicense = cLicense;
+    }
+
+    public Double getTotalKm() {
+        return totalKm;
+    }
+
+    public void setTotalKm(Double totalKm) {
+        this.totalKm = totalKm;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
